bb17a9af97d8129e512c25725edb7aa61ff69387
udayakp <kpu1990@gmail.com>
2018-08-31 23:58:05 +0530

    Add files via upload

---
+++ wsgi.py
@@ -0,0 +1,63 @@
+#from flask import Flask, render_template, request, jsonify
+#import aimodule
+#application = Flask(__name__)
+
+#@application.route("/")
+#def hello():
+#    aimodule.train()
+#    return render_template('chat.html')
+#    
+#@application.route("/ask", methods=['POST'])
+#def ask():
+#    # kernel now ready for use
+#    while True:
+#        message = str(request.form['messageText'])
+#        if message == "quit":
+#            aimodule.record()
+#        elif message == "save":
+#            aimodule.saveBrain("bot_brain.brn")
+#        elif message == "#elp":
+#            aimodule.incident()
+#        else:
+#            bot_response = aimodule.respond(message)
+#            # print bot_response
+#            return jsonify({'status':'OK','answer':bot_response})
+
+
+#if __name__ == "__main__":
+#    application.run()
+from flask import Flask, render_template, request, jsonify
+import aimodule
+import aimfmodule
+import requests
+import json
+application = Flask(__name__)
+url = "http://10.123.79.234:9081/zosapiconnect/fetchapplicant/"
+@application.route("/")
+def hello():
+    aimodule.train()
+    return render_template('chat.html')
+    
+@application.route("/ask", methods=['POST'])
+def ask():
+    # kernel now ready for use
+    while True:
+        message = str(request.form['messageText'])
+        m1 = message[:5]
+        if message == "quit":
+            aimodule.record()
+        elif message == "save":
+            aimodule.saveBrain("bot_brain.brn")
+        elif message == "#elp":
+            aimodule.incident()
+        elif m1 == "fetch":
+            bot_response = aimfmodule.fetch(url,message)
+            return jsonify({'status':'OK','answer':bot_response})
+        else:
+            bot_response = aimodule.respond(message)
+            # print bot_response
+            return jsonify({'status':'OK','answer':bot_response})
+
+
+if __name__ == "__main__":
+    application.run()
